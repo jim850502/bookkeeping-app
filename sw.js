8f187fd063e25253645f8affbc746bcff0c828ae
jim850502 <jim850502@gmail.com>
2026-09-16 13:39:44 +0800

    Refresh PWA cache for improved importer

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='driver-bookkeeping-v1';
+const CACHE='driver-bookkeeping-v2';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg']))));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
