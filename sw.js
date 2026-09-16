7ec6e5455c00ea62f0f32f51e04293dd6b5fc24d
jim850502 <jim850502@gmail.com>
2026-09-16 14:47:39 +0800

    Cache Taxiii read-only sync adapter

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-const CACHE='driver-bookkeeping-v2';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg']))));
+const CACHE='driver-bookkeeping-v3';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg','./taxiii-sync-adapter.js']))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
