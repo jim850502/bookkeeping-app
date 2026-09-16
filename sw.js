f9f5b9e407f10a512abfb440e8d9b4a05c0b66a6
jim850502 <jim850502@gmail.com>
2026-09-16 09:20:30 +0800

    Add offline support

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE='driver-bookkeeping-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg']))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
