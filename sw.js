26535a2deb06fa2644abcc5330b4ee0420b33fbe
jim850502 <jim850502@gmail.com>
2026-09-16 17:07:37 +0800

    Cache Taxiii sync panel and refresh PWA assets

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='driver-bookkeeping-v3';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg','./taxiii-sync-adapter.js']))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const CACHE='driver-bookkeeping-v4';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./taxiii-sync-adapter.js','./taxiii-sync-panel.html'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{if(e.request.method==='GET'&&r.ok){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>caches.match(e.request))));
