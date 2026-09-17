d95bf48a0630f40b70d050f273d0aed1ee2f271e
jim850502 <jim850502@gmail.com>
2026-09-17 14:45:51 +0800

    Cache Taxiii fare audit page and refresh PWA cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='driver-bookkeeping-v6';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./taxiii-sync-adapter.js','./taxiii-sync-panel.html','./taxiii-details.html'];
+const CACHE='driver-bookkeeping-v7';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./taxiii-sync-adapter.js','./taxiii-sync-panel.html','./taxiii-details.html','./taxiii-fare-audit.html'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{if(r.ok){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>caches.match(e.request))) });
